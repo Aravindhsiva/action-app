ee1921522a200b1f202c242410bba9106132aa2a
Aravindhsiva <adamaravindh@gmail.com>
2024-07-30 18:20:24 +0530

    Updating DEV Branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Build Works using React App
+          Code in DEV Branch
         </p>
         <a
           className="App-link"
